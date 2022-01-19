e9dfc69109009ab56a78418a7176f956d16f7525
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-01-19 17:59:48 -0500

    READ DESC
    We'll need to use world.sendEntityStatus() to get animations to work, but that method is broken.
    TODO: Someone please help with Minecraft's half-broken stuff we need to get it to work for the values to send to the client

--- src/main/java/frozenblock/wild/mod/entity/WardenEntityRenderer.java
+++ src/main/java/frozenblock/wild/mod/entity/WardenEntityRenderer.java
@@ -1,36 +1,27 @@
 package frozenblock.wild.mod.entity;
 
-import frozenblock.wild.mod.WildMod;
-import frozenblock.wild.mod.WildModClient;
-import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.MobEntityRenderer;
-import net.minecraft.util.Formatting;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
+import javax.annotation.Nullable;
+
 public class WardenEntityRenderer extends GeoEntityRenderer<WardenEntity> {
-    public ExampleGeoRenderer(EntityRenderDispatcher renderManager)
-    {
+
+    public WardenEntityRenderer(EntityRendererFactory.Context renderManager) {
         super(renderManager, new WardenEntityModel());
-        this.shadowRadius = 0.7F; //change 0.7 to the desired shadow size.
+        this.shadowRadius = 0.7F;
     }
-    private static final Identifier WARDEN_TEXTURE = new Identifier(WildMod.MOD_ID, "textures/entity/warden/warden.png");
-    private static final Identifier SECRET_WARDEN_TEXTURE = new Identifier(WildMod.MOD_ID, "textures/entity/warden/secret_warden.png");
 
-
-    public WardenEntityRenderer(EntityRendererFactory.Context context) {
-        super(context, new WardenEntityModel(context.getPart(WildModClient.MODEL_WARDEN_LAYER)), 0.5f);
-        this.addFeature(new WardenEntitySoulsFeatureRenderer(this));
-        this.addFeature(new WardenEntityOverlayFeatureRenderer(this));
-    }
 
     @Override
-    public Identifier getTexture(WardenEntity entity) {
-        String string = Formatting.strip(entity.getName().getString());
-        if (string != null && "Osmiooo".equals(string)) {
-            return SECRET_WARDEN_TEXTURE;
-        }
-            return WARDEN_TEXTURE;
+    public RenderLayer getRenderType(WardenEntity animatable, float partialTicks, MatrixStack stack,
+                                     @Nullable VertexConsumerProvider renderTypeBuffer, @Nullable VertexConsumer vertexBuilder,
+                                     int packedLightIn, Identifier textureLocation) {
+        return RenderLayer.getEntityCutout(this.getTextureLocation(animatable));
     }
 }
