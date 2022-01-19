42d4e277c94f89c4addbc9790b01c6a7d2731ece
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-01-19 17:52:17 -0500

    Update WardenEntityOverlayFeatureRenderer.java

--- src/main/java/frozenblock/wild/mod/entity/WardenEntityOverlayFeatureRenderer.java
+++ src/main/java/frozenblock/wild/mod/entity/WardenEntityOverlayFeatureRenderer.java
@@ -1,22 +1,33 @@
 package frozenblock.wild.mod.entity;
 
 import frozenblock.wild.mod.WildMod;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.entity.feature.EyesFeatureRenderer;
-import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
+import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
+import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
 
-@Environment(EnvType.CLIENT)
-public class WardenEntityOverlayFeatureRenderer extends EyesFeatureRenderer<WardenEntity, WardenEntityModel> {
-    private static final RenderLayer OVERLAY = RenderLayer.getEyes(new Identifier(WildMod.MOD_ID, "textures/entity/warden/warden_overlay.png"));
-
-    public WardenEntityOverlayFeatureRenderer(FeatureRendererContext<WardenEntity, WardenEntityModel> featureRendererContext) {
-        super(featureRendererContext);
+@SuppressWarnings("rawtypes")
+public class WardenEntityOverlayFeatureRenderer extends GeoLayerRenderer {
+    private static final Identifier MODEL = new Identifier(WildMod.MOD_ID, "geo/entity/warden.geo.json");
+    private static final Identifier OVERLAY = new Identifier(WildMod.MOD_ID, "textures/entity/warden/warden_overlay.png");
+    public WardenEntityOverlayFeatureRenderer(IGeoRenderer entityRendererIn) {
+        super(entityRendererIn);
     }
 
-    public RenderLayer getEyesTexture() {
-        return OVERLAY;
+    @Override
+    public void render(MatrixStack matrixStackIn, VertexConsumerProvider bufferIn, int packedLightIn, Entity entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+        RenderLayer eyes =  RenderLayer.getEyes(OVERLAY);
+        matrixStackIn.push();
+        //Move or scale the model as you see fit
+        matrixStackIn.scale(1.0f, 1.0f, 1.0f);
+        matrixStackIn.translate(0.0d, 0.0d, 0.0d);
+        this.getRenderer().render(this.getEntityModel().getModel(MODEL), entitylivingbaseIn, partialTicks, eyes, matrixStackIn, bufferIn,
+                bufferIn.getBuffer(eyes), packedLightIn, OverlayTexture.DEFAULT_UV, 1f, 1f, 1f, 1f);
+        matrixStackIn.pop();
     }
+
 }
